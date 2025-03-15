674aa876afd2d6bb4124fde75e72414af2424e8f
Masarvictor <u.victor.t@gmail.com>
2025-03-15 05:07:58 +0400

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,8 @@ def connect_telegram():
 def connect_linkedin():
     return "<h3>✅ تم الاتصال بـ LinkedIn!</h3><a href='/'>🔙 الرجوع</a>"
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
